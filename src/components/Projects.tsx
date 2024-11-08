9da4ebc5be406624168aba3ed9785a8cd18713ff
fhomaia <fhomaia@gmail.com>
2024-11-08 15:13:08 -0300

    fix remove extra modal

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -61,23 +61,6 @@ const Projects: React.FC = () => {
           return <Card project={project} duration={1 + (projects.length - (i + 1))*0.25} initial={-300 + (projects.length - (i + 1))/(projects.length)*(-300) } key={i}/>
         })
       }
-      <div className="modal fade" id="exampleModal" tab-index="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              ...
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
